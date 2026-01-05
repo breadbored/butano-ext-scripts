63404a32b6a10f8485616a6d9b6a8d642efadbe1
Jason Carpenter <5916026+breadbored@users.noreply.github.com>
2026-01-04 21:52:20 -0500

    better example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -12,26 +12,42 @@
 #include "bn_sprite_text_generator.h"
 
 #include "fonts/happy_16x16_font.hpp"
+#include "fonts/kindergarden_32x32_font.hpp"
 
 namespace
 {
-    constexpr bn::fixed text_y_inc = 14;
+    constexpr bn::fixed text_y_inc = 32;
     constexpr bn::fixed text_y_limit = (bn::display::height() / 2) - text_y_inc;
 
-    void text_scene()
-    {
-        bn::sprite_text_generator text_generator(fonts::happy_16x16_sprite_font);
-        text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> text_sprites;
-        text_generator.generate(0, -text_y_limit, "Text", text_sprites);
-        text_generator.generate(0, text_y_limit, "START: go to next scene", text_sprites);
-
-        while(! bn::keypad::start_pressed())
+        void happy_text_scene()
         {
-            bn::core::update();
+            bn::sprite_text_generator text_generator(fonts::happy_16x16_sprite_font);
+            text_generator.set_center_alignment();
+
+            bn::vector<bn::sprite_ptr, 32> text_sprites;
+            text_generator.generate(0, -text_y_limit, "Happy", text_sprites);
+            text_generator.generate(0, text_y_limit, "PRESS START", text_sprites);
+
+            while(! bn::keypad::start_pressed())
+            {
+                bn::core::update();
+            }
         }
-    }
+
+            void kindergarden_text_scene()
+            {
+                bn::sprite_text_generator text_generator(fonts::kindergarden_32x32_sprite_font);
+                text_generator.set_center_alignment();
+
+                bn::vector<bn::sprite_ptr, 32> text_sprites;
+                text_generator.generate(0, -text_y_limit, "Kindergarden", text_sprites);
+                text_generator.generate(0, text_y_limit, "PRESS START", text_sprites);
+
+                while(! bn::keypad::start_pressed())
+                {
+                    bn::core::update();
+                }
+            }
 }
 
 int main()
@@ -42,7 +58,10 @@ int main()
 
     while(true)
     {
-        text_scene();
+        happy_text_scene();
+        bn::core::update();
+
+        kindergarden_text_scene();
         bn::core::update();
     }
 }
